a94610f8993897f5a7d251cf5550c39aee64bcbe
Ben Sheffield <me@mrbenshef.co.uk>
2020-06-27 19:06:58 +0100

    ci: back to basics

--- docker/aarch64-unknown-linux-gnu.Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM rustembedded/cross:aarch64-unknown-linux-gnu
-
-RUN dpkg --add-architecture arm64 && \
-    apt-get update && \
-    apt-get install --assume-yes libglib2.0-dev:arm64 libgtk-3-dev:arm64 libgtksourceview-3.0-dev:arm64
--- docker/docker-build.sh
+++
@@ -1,8 +0,0 @@
-# Builds docker containers for cross compilation
-
-SCRIPT=`realpath $0`
-SCRIPTPATH=`dirname $SCRIPT`
-
-docker build -t boop-gtk/aarch64-unknown-linux-gnu -f $SCRIPTPATH/aarch64-unknown-linux-gnu.Dockerfile $SCRIPTPATH
-docker build -t boop-gtk/x86_64-unknown-linux-gnu -f $SCRIPTPATH/x86_64-unknown-linux-gnu.Dockerfile $SCRIPTPATH
-docker build -t boop-gtk/x86_64-pc-windows-gnu -f $SCRIPTPATH/x86_64-pc-windows-gnu.Dockerfile $SCRIPTPATH
