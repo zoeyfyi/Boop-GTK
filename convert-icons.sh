838c9177814483249a384bef9d8197ff8786fe26
Ben Sheffield <bshefb@gmail.com>
2020-07-10 03:49:15 +0100

    chore: fix icon conversion producing invalid names
    For files not starting with icons8

--- convert-icons.sh
+++ convert-icons.sh
@@ -3,8 +3,10 @@ rm ui/icons/*.svg;
 for f in submodules/Boop/Boop/Boop/Assets.xcassets/Icons/**/*.pdf; do 
 echo "Processing $f file..";
 name="$(basename -- ${f})";
-name="${name:7}";
+name="${name#icons8-}";
 name="${name%.pdf}";
 name="$name.svg";
 pdf2svg $f ui/icons/$name;
+echo "  Created ui/icons/$name"
+echo "   "
 done
